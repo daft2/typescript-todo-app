8e385674d72c8b5a6bebe80ce8c38e3801956958
Daft <flushid@gmail.com>
2020-12-28 21:46:22 +0700

    implementing union and optional field

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import './App.css';
 
+type Place = 'home' | 'work' | { custom: string }
+
 type Todo = Readonly<{
   id: number
   text: string
   done: boolean
+  place?: Place
 }>
 
 type CompletedTodo = Todo & {
   readonly done: true
 }
 
-const item: Todo = {
-  id: 1,
-  text: "First Todo",
-  done: false
-}
+const items = [
+  {
+    id: 1,
+    text: "First Todo",
+    done: false
+  },
+  {
+    id: 2,
+    text: "Second Todo",
+    done: false
+  }
+]
 
 function toggleTodo(todo: Todo): Todo {
   return {
@@ -32,10 +42,17 @@ function completeAll(todos: readonly Todo[]): CompletedTodo[] {
   }))
 }
 
+
 function App() {
   return (
     <>
-      <p>Hello</p>
+      {items.map(item => (
+        <div>
+          <input type="checkbox" id="checkboxName" />
+          <label htmlFor="checkboxName">{item.text}</label>
+        </div>
+      ))}
+      <input type="button" value="mark all" />
     </>
   );
 }
